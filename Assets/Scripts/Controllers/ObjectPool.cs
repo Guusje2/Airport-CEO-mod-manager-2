3f59be9b0173aa5a07c4e21e15a8ebb048bd6a86
Guus Dohmen <guusdohmen@hotmail.nl>
2020-01-11 22:56:49 +0100

    Working on oracle db stuff

---
+++ Assets/Scripts/Controllers/ObjectPool.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool instance;
+
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool = 500;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        //1
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            //2
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        //3   
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Controllers/OracleDBController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;      
+using Oracle.ManagedDataAccess.Client;
+
+public class OracleDBController : MonoBehaviour
+{
+    OracleCommand cmd;
+    string conString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Enter user id and password, such as ADMIN user	
+        conString = "User Id=ADMIN;Password=4vHhjPm2PM26FLr;" +
+
+        //Enter net service name for data source value
+        "Data Source=ACEOMMDB_HIGH";
+        GetData();
+    }
+
+    public void GetData() {
+        using (OracleConnection con = new OracleConnection(conString)) 
+        {
+            con.Open();
+
+            cmd = con.CreateCommand();
+            Debug.Log("connected?");
+            OracleParameter param = cmd.Parameters.Add("BlobParameter", OracleDbType.Blob);
+            param.Direction = System.Data.ParameterDirection.Output;
+            
+
+            cmd.CommandText = "SELECT * FROM BUSINESSES";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            Debug.Log(reader.GetString(0));
+            reader.Close();
+        }
+    }
+}
